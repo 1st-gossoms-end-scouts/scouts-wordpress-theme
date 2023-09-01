1de0a4a4191e209b8e2f2c7aff3dd13b53afb259
Harvey Allsop <49364385+hallsop@users.noreply.github.com>
2023-09-01 12:43:56 +0800

    Bugfix/fix font loading (#7)

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -11,13 +11,17 @@ if ( ! function_exists('b5st_enqueues') ) {
 		wp_register_style('bootstrapIcons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css', false, '1.9.1', null);
 		wp_enqueue_style('bootstrapIcons');
 
+        wp_register_style('google-fonts', 'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,900', false, null);
+        wp_enqueue_style( 'google-fonts');
+
 		wp_register_style('theme', get_template_directory_uri() . '/theme/css/b5st.css', false, null);
 		wp_enqueue_style('theme');
 
-		// Scripts
+        // Scripts
 
 		wp_register_script('bootstrap5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js', false, '5.2.1', true);
 		wp_enqueue_script('bootstrap5');
+
 
 		if (is_singular() && comments_open() && get_option('thread_comments')) {
 			wp_enqueue_script('comment-reply');
--- functions/gutenberg.php
+++ functions/gutenberg.php
@@ -1,6 +1,8 @@
 <?php
 function myguten_register_meta()
 {
+    wp_register_style('google-fonts', 'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,900', false, null);
+    wp_enqueue_style( 'google-fonts');
     wp_enqueue_script( 'guten', get_template_directory_uri() . '/theme/js/gutenberg.js', [ 'wp-element', 'wp-hooks' , 'wp-compose', 'wp-data', 'wp-components', 'wp-block-editor'] );
     register_meta('post', 'align_featured_image', array(
         'show_in_rest' => true,
