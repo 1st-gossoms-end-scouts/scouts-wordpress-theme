7cd92d51bf7f4a4a9bc6b31819c3ca956dde0906
Harvey Allsop <harvey@harveyallsop.com>
2023-09-03 17:50:58 +0800

    Auto register widget as default, remove default widgets from other areas

--- functions/widgets.php
+++ functions/widgets.php
@@ -3,6 +3,7 @@
  * Widgets
  */
 
+
 function b5st_widgets_init() {
 
   /**
@@ -27,7 +28,7 @@ function b5st_widgets_init() {
     'name'            => __( 'Mainbody Widget Area', 'scouts-wordpress-theme' ),
     'id'              => 'mainbody-widget-area-1',
     'description'     => __( 'Use 1, 2, 3 or 4 widgets.', 'scouts-wordpress-theme' ),
-    'before_widget'   => '<div class="%1$s %2$s col-md">',
+    'before_widget'   => '<div id="%1$s" class="%2$s col-md">',
     'after_widget'    => '</div>',
     'before_title'    => '<h2 class="h4">',
     'after_title'     => '</h2>',
@@ -41,13 +42,41 @@ function b5st_widgets_init() {
     'name'            => __( 'Footer Widget Area', 'scouts-wordpress-theme' ),
     'id'              => 'footer-widget-area',
     'description'     => __( 'Use 1, 2, 3 or 4 widgets.', 'scouts-wordpress-theme' ),
-    'before_widget'   => '<div class="%1$s %2$s col-md">',
+    'before_widget'   => '<div id="%1$s" class="%2$s col-md">',
     'after_widget'    => '</div>',
     'before_title'    => '<h2 class="h4">',
     'after_title'     => '</h2>',
   ) );
-
 }
+
 add_action( 'widgets_init', 'b5st_widgets_init' );
 
 
+
+function set_widgets() {
+    // Get all the associated widgets;
+    $sidebar_widgets = get_option ( 'sidebars_widgets' );
+
+    // Check this specific sidebar
+    if ( isset( $sidebar_widgets [ 'mainbody-widget-area-1' ] ) ) {
+        unset ( $sidebar_widgets [ 'mainbody-widget-area-1' ] );
+        // Update the option
+        update_option ( 'sidebars_widgets', $sidebar_widgets );
+    }
+
+    if ( isset( $sidebar_widgets [ 'footer-widget-area' ] ) ) {
+        $widget_instances = get_option( 'widget_block', array() );
+        $numeric_keys = array_filter( array_keys( $widget_instances ), 'is_int' );
+        $next_key = $numeric_keys ? max( $numeric_keys ) + 1 : 2;
+        // Add this widget to the sidebar
+        $sidebar_widgets [ 'footer-widget-area' ] = array( 'block-' . $next_key );
+        // Add the new widget instance
+        $widget_instances[ $next_key ] = array('content' => '<!-- wp:scouts-wordpress-theme/scout-footer-widget /-->');
+        // Update the option
+        update_option( 'widget_block', $widget_instances);
+        update_option ( 'sidebars_widgets', $sidebar_widgets );
+    }
+}
+
+
+add_action( 'after_switch_theme', 'set_widgets' );
--- theme/blocks/src/scout-footer-widget/render.php
+++ theme/blocks/src/scout-footer-widget/render.php
@@ -4,7 +4,7 @@
  */
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?> id="scout-footer-widget-div">
-    <img style="max-width: 7em;" src="<?php echo get_theme_file_uri('theme/assets/fleur-de-lis-marque-white.png') ?>" alt="Scout Logo">
+    <img style="max-width: 5em;" src="<?php echo get_theme_file_uri('theme/assets/fleur-de-lis-marque-white.png') ?>" alt="Scout Logo">
     <div class="widget_scout_footer_column_div">
         <div class="widget_scout_footer_text_left">
             <?php echo strip_tags($attributes['leftContent'], '<br><strong><a>'); ?>
