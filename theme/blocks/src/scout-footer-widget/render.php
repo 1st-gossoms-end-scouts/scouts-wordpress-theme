ddf9d19282378ae23f62d4d8709a26be8e222e63
Harvey Allsop <harvey@harveyallsop.com>
2023-09-03 18:22:04 +0800

    Fix colours in other colour schemes

--- theme/blocks/src/scout-footer-widget/render.php
+++ theme/blocks/src/scout-footer-widget/render.php
@@ -4,13 +4,18 @@
  */
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?> id="scout-footer-widget-div">
-    <img style="max-width: 5em;" src="<?php echo get_theme_file_uri('theme/assets/fleur-de-lis-marque-white.png') ?>" alt="Scout Logo">
+    <?php if ($GLOBALS['pallete'][4] == "#FFFFFF") {
+        $footer_logo_img_src = 'theme/assets/fleur-de-lis-marque-white.png';
+    } else {
+        $footer_logo_img_src = 'theme/assets/fleur-de-lis-marque-black.png';
+    } ?>
+    <img style="max-width: 5em;" src="<?php echo get_theme_file_uri($footer_logo_img_src) ?>" alt="Scout Logo">
     <div class="widget_scout_footer_column_div">
-        <div class="widget_scout_footer_text_left">
+        <div class="widget_scout_footer_text_left footer-text">
             <?php echo strip_tags($attributes['leftContent'], '<br><strong><a>'); ?>
         </div>
-        <div class="widget_scout_vertical_line"></div>
-        <div class="widget_scout_footer_text_right">
+        <div style="border-left: thick solid <?php echo $GLOBALS['pallete'][4] ?>"></div>
+        <div class="widget_scout_footer_text_right footer-text">
             <?php echo strip_tags($attributes['rightContent'], '<br><strong><a>'); ?>
         </div>
     </div>
